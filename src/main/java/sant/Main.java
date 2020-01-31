74aef823d5d679b96efa297d9c075428f967f208
Keaton <san16072@byui.edu>
2020-01-31 13:33:43 -0700

    Revert "entered bad code"
    This reverts commit 7e6046b11554dd1c27511c7b356b436f8a23c3c6.

--- src/main/java/sant/Main.java
+++ src/main/java/sant/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello my name is your Keaton!");
         System.out.println("Hello my Keaton! I am the Daniel");
-        bad code
     }
 }
