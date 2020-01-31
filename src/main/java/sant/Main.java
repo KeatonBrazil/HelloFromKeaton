7e6046b11554dd1c27511c7b356b436f8a23c3c6
Keaton <san16072@byui.edu>
2020-01-31 13:31:27 -0700

    entered bad code

--- src/main/java/sant/Main.java
+++ src/main/java/sant/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello my name is your Keaton!");
         System.out.println("Hello my Keaton! I am the Daniel");
+        bad code
     }
 }
